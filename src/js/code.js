125dbce7bc17924f5fbfe4f5d40e5c636bfb32da
Sash <sash@binary.com>
2018-10-04 10:45:29 +0300

    update

--- src/js/code.js
+++ src/js/code.js
@@ -7,13 +7,13 @@ $("#sun").mouseout(function() {
   //TweenMax.to("#sun11-window *", 0.5, {fill:"#F8A805", ease: Power4.easeInOut});
 });
 
-$("#Board ").mouseover(function() {
-  TweenMax.to("#BTC .cls-8", 0.5, {stroke:"red", ease: Power4.easeInOut});
+$("#Headphone").mouseover(function() {
+  TweenMax.to("#Headphone *", 0.5, {stroke:"red", ease: Power4.easeInOut});
   TweenMax.to("#Web_Designer #Board", 0.5, {x:-1000, y:0, scale:3,  ease: Power4.easeInOut});
   TweenMax.to("#DeskTop ", 0.5, {opacity:0,  ease: Power4.easeInOut});
   TweenMax.to("#sunT .test", 0.5, {fill:"red", ease: Power4.easeInOut});
 });
-$("#Board").mouseout(function() {
+$("#Headphone").mouseout(function() {
   TweenMax.to("#BTC .cls-8", 0.5, {stroke:"red", ease: Power4.easeInOut});
   TweenMax.to("#Web_Designer #Board", 0.5, {x:0, y:0, scale:1,  ease: Power4.easeInOut});
   TweenMax.to("#DeskTop ", 0.5, {opacity:1,  ease: Power4.easeInOut});
@@ -50,9 +50,9 @@ $("#bird").mouseover(function() {
 });
 
 
-  $('#accordion').accordion({
-      heightStyle: 'content',
-      collapsible: true,
-      active     : false,
+$('#accordion').accordion({
+    heightStyle: 'content',
+    collapsible: true,
+    active     : false,
 
-  });
+});
