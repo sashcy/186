7af6db6d7a9f67481316450ed22e6106ddcce41c
Sash <sash@binary.com>
2018-10-23 22:13:59 +0300

    update build

--- grunt/svgo.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    all: {
-        files: [
-            { expand: true, cwd: 'src/assets/', src: ['usb_page/*.svg'],                           dest: 'dist/assets/' },
-        ],
-    }
-};
