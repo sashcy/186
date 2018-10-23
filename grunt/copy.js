fefe644d9019027d2b141e5d9ceada79da2b9120
Sash <sash@binary.com>
2018-10-23 21:46:47 +0300

    update

--- grunt/copy.js
+++ grunt/copy.js
@@ -4,9 +4,4 @@ module.exports = {
             { expand: true, cwd: 'src/',  src: ['*.md', '*.html'],                           dest: 'dist' },
         ],
     },
-    svg: {
-        files: [
-            { expand: true, cwd: 'src/assets/', src: ['svg/**'],                           dest: 'dist/assets' },
-        ],
-    },
 };
---
+++ grunt/svgo.js
@@ -0,0 +1,7 @@
+module.exports = {
+    all: {
+        files: [
+            { expand: true, cwd: 'src/assets/', src: ['usb_page/*.svg'],                           dest: 'dist/assets/' },
+        ],
+    }
+};
